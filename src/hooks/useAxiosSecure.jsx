df7bdca4ded224f78762f267b553b249f633fe71
Md Soriful hasan <sorifullhasan300@gmail.com>
2025-07-11 14:35:30 +0600

    create axios instance for pass access token and secure API

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,12 +1,48 @@
 import axios from "axios";
-import React from "react";
+import useAuth from "./useAuth";
+import { useEffect } from "react";
+import Swal from "sweetalert2";
 
+const axiosInstance = axios.create({
+  baseURL: `${import.meta.env.VITE_url}`,
+});
 const useAxiosSecure = () => {
-  const axiosSecure = axios.create({
-    baseURL: `${import.meta.env.VITE_url}`,
-  });
-
-  return axiosSecure;
+  const { user, signOut, loading } = useAuth();
+  useEffect(() => {
+    if (!loading && user?.accessToken) {
+      // request interceptor
+      const requestInterCeptor = axiosInstance.interceptors.request.use(
+        (config) => {
+          config.headers.Authorization = `Bearer ${user?.accessToken}`;
+          return config;
+        }
+      );
+      // response interceptor
+      const responseInterCeptor = axiosInstance.interceptors.response.use(
+        (res) => res,
+        (err) => {
+          if (err?.response?.status === 401 || err?.response?.status === 403) {
+            signOut()
+              .then(() => {
+                Swal.fire({
+                  title: "Logged out due to token issue",
+                  icon: "error",
+                  draggable: true,
+                });
+              })
+              .catch(console.error);
+          }
+          return Promise.reject(err);
+        }
+      );
+      // Cleanup to prevent multiple interceptors on re-renders
+      return () => {
+        axiosInstance.interceptors.request.eject(requestInterCeptor);
+        axiosInstance.interceptors.response.eject(responseInterCeptor);
+      };
+    }
+  }, [loading, signOut, user]);
+  return axiosInstance;
 };
 
 export default useAxiosSecure;
